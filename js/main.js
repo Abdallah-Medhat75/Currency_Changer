b3ff9ee430fe6113755dd9eeab622f35221099a1
unknown <abdallah.medhata3@gmail.com>
2025-04-28 19:46:06 +0300

    Added More Currencies To Convert To Egyptian Pound

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
-myDiv = document.querySelector("div");
-myInput = document.querySelector("input");
+let myDiv = document.querySelector("div");
+let myInput = document.querySelector("input");
+let mySelect = document.querySelector("select");
+
+
 
 document.oninput = function () {
-    myDiv.innerHTML = `${myInput.value} USD Dollars is ${(myInput.value * 40).toFixed(2)} Egyptian Pound`;
+    if (myInput.value != "")
+    {
+        if (mySelect.value == "USD") {
+            myDiv.innerHTML = `${myInput.value} USD Dollars is ${(myInput.value * 50.38).toFixed(2)} Egyptian Pound`;
+        } else if (mySelect.value == "EUR") {
+            myDiv.innerHTML = `${myInput.value} EURO Euros is ${(myInput.value * 57.89).toFixed(2)} Egyptian Pound`;
+        } else if (mySelect.value == "GBP") {
+            myDiv.innerHTML = `${myInput.value} England Pounds is ${(myInput.value * 68.10).toFixed(2)} Egyptian Pound`;
+        } else if (mySelect.value == "SAR") {
+            myDiv.innerHTML = `${myInput.value} Saudi Reals is ${(myInput.value * 13.55).toFixed(2)} Egyptian Pound`;
+        } else if (mySelect.value == "AED") {
+            myDiv.innerHTML = `${myInput.value} Emirates Currency is ${(myInput.value * 13.84).toFixed(2)} Egyptian Pound`;
+        }    
+    }
 }
